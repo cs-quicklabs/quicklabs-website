3bfa68a2e44dfc08d62a9806fb4f6d330bf82015
Aashish Dhawan <aashishdhawan@gmail.com>
2023-08-24 22:00:30 +0530

    UI fixes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,24 @@
-<div class="mx-auto max-w-7xl py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
-	<div class="flex flex-col items-center justify-center">
-		<div class="flex w-full flex-col">
-			<h1
-				class="mb-4 text-5xl font-extrabold leading-none tracking-tight text-black drop-shadow dark:text-white sm:mt-6 sm:text-7xl lg:font-black lg:leading-tight"
-				data-cy="primary-heading"
-			>
-				<span
-					class="rounded-lg bg-gradient-to-r from-pink-500 to-fuchsia-500 bg-clip-text text-transparent"
-					>Quicklabs.</span
+<div
+	class="h-screen bg-white text-slate-700 antialiased selection:bg-blue-500 selection:text-white dark:bg-slate-900 dark:text-slate-400 dark:selection:bg-sky-500"
+>
+	<div class="mx-auto max-w-7xl py-12 px-4 sm:px-12 lg:py-16 lg:px-8">
+		<div class="flex flex-col items-center justify-center">
+			<div class="flex w-full flex-col">
+				<h1
+					class="mb-4 text-5xl font-extrabold leading-none tracking-tight text-black drop-shadow dark:text-white sm:mt-6 sm:text-7xl lg:font-black lg:leading-tight"
+					data-cy="primary-heading"
 				>
-			</h1>
-			<h2 class="max-w-2xl text-2xl font-semibold drop-shadow dark:text-slate-100 sm:mb-11">
-				Your Business, Our Software. Let's Build.
-			</h2>
-			<div class="flex flex-wrap space-y-4 sm:space-y-0 sm:space-x-4">
-				<p class="text-sm text-gray-500">Launching soon...</p>
+					<span
+						class="rounded-lg bg-gradient-to-r from-pink-500 to-fuchsia-500 bg-clip-text text-transparent"
+						>Quicklabs.</span
+					>
+				</h1>
+				<h2 class="max-w-2xl mb-8 text-2xl font-semibold drop-shadow dark:text-slate-100 sm:mb-12">
+					Your Business, Our Software. Let's Build.
+				</h2>
+				<div class="flex flex-wrap space-y-4 sm:space-y-2 sm:space-x-4">
+					<p class="text-sm text-gray-500 dark:text-slate-100">Launching soon...</p>
+				</div>
 			</div>
 		</div>
 	</div>
