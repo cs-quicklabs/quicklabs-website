60f090b31c0bd0a522d848b436fba078b822413e
Aashish Dhawan <aashishdhawan@gmail.com>
2023-08-24 21:22:18 +0530

    initial commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="mx-auto max-w-7xl py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
+	<div class="flex flex-col items-center justify-center">
+		<div class="flex w-full flex-col">
+			<h1
+				class="mb-4 text-5xl font-extrabold leading-none tracking-tight text-black drop-shadow dark:text-white sm:mt-6 sm:text-7xl lg:font-black lg:leading-tight"
+				data-cy="primary-heading"
+			>
+				<span
+					class="rounded-lg bg-gradient-to-r from-pink-500 to-fuchsia-500 bg-clip-text text-transparent"
+					>Quicklabs.</span
+				>
+			</h1>
+			<h2 class="max-w-2xl text-2xl font-semibold drop-shadow dark:text-slate-100 sm:mb-11">
+				Your Business, Our Software. Let's Build.
+			</h2>
+			<div class="flex flex-wrap space-y-4 sm:space-y-0 sm:space-x-4">
+				<p class="text-sm text-gray-500">Launching soon...</p>
+			</div>
+		</div>
+	</div>
+</div>
